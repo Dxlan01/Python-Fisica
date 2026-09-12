35467fb8e27c3f6fd94efa8061ee250caa2f141c
Dxlan01 <Dylancalero81@gmail.com>
2026-09-11 21:20:30 -0500

    6 y 7

---
+++ Practica/P6.py
@@ -0,0 +1,4 @@
+masa=float(input("Ingrese la masa: "))
+fuerza=float(input("Ingrese la fuerza"))
+aceleracion= fuerza/masa
+print(f"La aceleracion es: {aceleracion}")
---
+++ Practica/P7.py
@@ -0,0 +1,4 @@
+distancia=float(input("Ingrese la distancia: "))
+tiempo=float(input("Ingrese el tiempo: "))
+velocidad= distancia/tiempo
+print(f"La velocidad es: {velocidad}")
